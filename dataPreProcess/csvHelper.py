95285076a5fd1b3bd5b69d276aa2b84d7c87a4b6
weskita <zkonline1989@163.com>
2015-05-11 17:52:34 +0800

    decision tree, 50% complete.

--- dataPreProcess/csvHelper.py
+++ dataPreProcess/csvHelper.py
@@ -1,4 +1,5 @@
 import csv
+import string
 import numpy
 def matrix2csv(lines,csv_file_path,title_line):
 	writer = csv.writer(file(csv_file_path, 'wb'))
@@ -20,6 +21,36 @@ def csv2matrix(csv_file_path, has_title_line, has_line_header):
 		
 	matrix = numpy.array(rawMatrix)
 	return matrix
+	
+def strVec2Numvec(strVec):
+	res = []
+	for item in strVec:
+		res.append(string.atof(item))
+	#print res
+	return res	
+	
+def readData(filePath, hasTitleLine, hasLineHeader):	
+	inputData = csv2matrix(filePath, hasTitleLine, hasLineHeader)
+	strMatrix = numpy.array(inputData)
+	size = strMatrix.shape
+	strSet = strMatrix[:,0:size[1] - 1]
+	featVecs = []
+	for item in strSet:
+		featVecs.append(strVec2Numvec(item))
+	markVec = strMatrix[:,size[1] - 1]
+	return [featVecs,markVec]
+	
+# Union the features vectors and mark vector into a matrix
+def createDataSet(featVecs,markVec):
+	res = []
+	i = 0
+	print featVecs[i]	
+	for mark in markVec:		
+		featVecs[i].append(mark)		
+		#print featVecs[i]
+		res.append(featVecs[i])
+		i+=1
+	return res
 		
-matrix2csv([[1,2,3],[4,5,6],[0,7,9]],"mycsv.csv",['a','b','c'])
-print csv2matrix("mycsv.csv",True,True)
+#matrix2csv([[1,2,3],[4,5,6],[0,7,9]],"mycsv.csv",['a','b','c'])
+#print csv2matrix("mycsv.csv",True,True)
---
+++ dataPreProcess/decisionTree.py
@@ -0,0 +1,55 @@
+from math import log
+from csvHelper import *
+import numpy
+
+#getShannonEnt(): parameter: dataSet, the last colume is mark vector; the other columes of this matrix are feature vectors.
+def getShannonEnt(dataSet):
+	numEntries = len(dataSet)
+	labelCounts = {}
+	for featVec in dataSet:
+		currentLabel = featVec[-1]
+		if currentLabel not in labelCounts.keys():
+			labelCounts[currentLabel] = 0
+		
+		labelCounts[currentLabel] += 1
+		
+	shannonEnt = 0.0
+	for key in labelCounts:
+		prob = float(labelCounts[key]) / numEntries
+		shannonEnt -= prob * log(prob,2)
+		
+	return shannonEnt
+
+	
+def splitDataSet(dataSet, axis, value):
+	retDataSet = []
+	for featVec in dataSet:
+		if featVec[axis] == value:
+			reducedFeatVec = featVec[:axis]
+			reducedFeatVec.extend(featVec[axis+1:])
+			retDataSet.append(reducedFeatVec)
+	return retDataSet
+	
+def discreteIntoBase4(vector):
+	upLimit = max(vector)
+	downLimit = min(vector)
+	sectionSize = (upLimit - downLimit) / 4
+	retVector = []
+	for num in vector:
+		retVector.append(int(round(num / sectionSize)))		
+	return retVector
+
+	
+[features,markVec] = readData('foodData.csv', True, True)
+discreteFeats = []
+for feat in features:
+	discreteFeats.append(discreteIntoBase4(feat))
+	
+dataSet = createDataSet(discreteFeats,markVec)
+
+#print dataSet
+print splitDataSet(dataSet,0,1)
+#print discreteFeats
+#print markVec
+
+	
